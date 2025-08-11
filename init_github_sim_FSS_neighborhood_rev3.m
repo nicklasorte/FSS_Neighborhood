3693f2f877cfb2893419ac947bcc213b4da345c4
Nick LaSorte <37697540+nicklasorte@users.noreply.github.com>
2025-08-11 08:40:48 -0400

    Add files via upload

---
+++ init_github_sim_FSS_neighborhood_rev3.m
@@ -0,0 +1,380 @@
+clear;
+clc;
+close all;
+app=NaN(1);  %%%%%%%%%This is to allow for Matlab Application integration.
+format shortG
+%format longG
+top_start_clock=clock;
+folder1='C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\7GHz FSS Neighborhoods';
+cd(folder1)
+addpath(folder1)
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\Basic_Functions')
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\General_Terrestrial_Pathloss')
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\General_Movelist')
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\3.1GHz Neighborhood') %%%%%%Rand Real Data
+addpath('C:\Users\nlasorte\OneDrive - National Telecommunications and Information Administration\MATLAB2024\Generic_Bugsplat')
+addpath('C:\Local Matlab Data\Local MAT Data') %%%%%%%One Drive Error with mat files
+pause(0.1)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%FSS
+%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Base Station Data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%1) Azimuth -180~~180
+%%%2) Rural
+%%%3) Suburban
+%%%4) Urban
+aas_zero_elevation_data=zeros(361,4);
+aas_zero_elevation_data(:,1)=-180:1:180;
+%%%%AAS Reduction in Gain to Max Gain (0dB is 0dB reduction)
+bs_down_tilt_reduction=abs(max(aas_zero_elevation_data(:,[2:4]))) %%%%%%%%Downtilt dB Value for Rural/Suburban/Urban
+norm_aas_zero_elevation_data=horzcat(aas_zero_elevation_data(:,1),aas_zero_elevation_data(:,[2:4])+bs_down_tilt_reduction);
+bs_down_tilt_reduction=min(bs_down_tilt_reduction)
+max(norm_aas_zero_elevation_data(:,[2:4])) %%%%%This should be [0 0 0]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Base Station Deployment
+load('rand_real_2025.mat','rand_real_2025')  %%%%%%%%1)Lat, 2)Lon, 3)Antenna Height
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%From the CBRS Portal Data: Pax River
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+cell_data_header=cell(1,32);
+cell_data_header{1}='data_label1';
+cell_data_header{2}='latitude';
+cell_data_header{3}='longitude';
+cell_data_header{4}='rx_bw_mhz';
+cell_data_header{5}='rx_height';
+cell_data_header{6}='ant_hor_beamwidth';
+cell_data_header{7}='min_azimuth';
+cell_data_header{8}='max_azimuth';
+cell_data_header{9}='rx_ant_gain_mb';
+cell_data_header{10}='rx_nf';
+cell_data_header{11}='in_ratio';
+cell_data_header{12}='min_ant_loss';
+cell_data_header{13}='fdr_dB';
+cell_data_header{14}='dpa_threshold';
+cell_data_header{15}='required_pathloss';
+cell_data_header{16}='base_protection_pts';
+cell_data_header{17}='base_polygon';
+cell_data_header{18}='gmf_num';
+cell_data_header{19}='rx_lat';
+cell_data_header{20}='rx_lon';
+cell_data_header{21}='base_polyshape';
+cell_data_header{22}='ant_diamter_m';
+cell_data_header{23}='Sat_ID';
+cell_data_header{24}='Noise_TempK';
+cell_data_header{25}='Ground_Elevation_m';
+cell_data_header{26}='Antenna_Pattern_Str';
+cell_data_header{27}='rx_if_bw_mhz';
+cell_data_header{28}='array_ant_pattern';  %%%Change this to tf_custom_ant_pattern
+cell_data_header{29}='TF_Custom_Ant_Pattern';
+cell_data_header{30}='X_POL_dB';
+cell_data_header{31}='gs_azimuth';
+cell_data_header{32}='gs_elevation';
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%WGS F1 (USA 195): -42.53 (Longitude): NORAD ID: 32258
+%%%WGS F5 (USA 243): -52.51 (Longitude): NORAD ID: 39168
+%%%WGS F6 (USA 244): -135.19 (Longitude): NORAD ID: 39222
+
+%%%38.8730° N, 77.0074 %%%%Nats Stadium
+
+cell_sim_data=cell(1,32);
+cell_sim_data{1,1}='NatsStadiumFSS';
+cell_sim_data{1,2}=38.8730; %%%%%Lat
+cell_sim_data{1,3}=-77.0074; %%%%Lon
+cell_sim_data{1,5}=10;  %%%%%Height
+cell_sim_data{1,6}=0.24; %%%%BW
+cell_sim_data{1,7}=0; %%%Double Zero should keep it fixed? (Non-rotating)
+cell_sim_data{1,8}=0; %%%Double Zero should keep it fixed? (Non-rotating)
+cell_sim_data{1,9}=58; %%%Ant Gain
+cell_sim_data{1,11}=-6; %%%I/N dB
+cell_sim_data{1,13}=0; %%%FDR dB
+cell_sim_data{1,16}=horzcat(cell_sim_data{1,2},cell_sim_data{1,3},cell_sim_data{1,5});
+cell_sim_data{1,17}=horzcat(cell_sim_data{1,2},cell_sim_data{1,3});
+cell_sim_data{1,22}=12.2; %%An Diameter meters
+cell_sim_data{1,23}='WGS F6 (USA 244)';
+cell_sim_data{1,24}=176.20;%%%%%Noise Temperature K
+cell_sim_data{1,26}='itu_antenna_appendix8_annex3';
+cell_sim_data{1,27}=60;%%%%Rx IF bandwidth MHz
+cell_sim_data{1,29}=1;%%%%tf_custom_ant_pattern (which means we will save it and pull it).
+cell_sim_data{1,30}=3;%%%%x_pol_dB 3dB
+cell_sim_data=vertcat(cell_data_header,cell_sim_data);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Test
+rev=989; %%%%%Nats Stadium Example
+freq_separation=0; %%%%%%%Assuming co-channel
+bs_eirp=50.5%%%% %%%%%EIRP [dBm/10MHz] for Rural, Suburan, Urban: 62dBm/1MHz --> 36.25dBm/MHz at 50th (0,0), then - 1.25 for 80% TDD, 35dBm/Mhz --> 45dBm/10MHz
+mitigation_dB=0:10:30;  %%%%%%%%% in dB%%%%% Beam Muting or PRB Blanking (or any other mitigation mechanism):  30 dB reduction %%%%%%%%%%%%Consider have this be an array, 3dB step size, to get a more granular insight into how each 3dB mitigation reduces the coordination zone.
+mc_size=1;%%%% Since we're at 50%
+tf_full_binary_search=1;  %%%%%Search all DPA Points, not just the max distance point
+min_binaray_spacing=2;%4%8; %%%%%%%minimum search distance (km)
+reliability=50%
+move_list_reliability=reliability;
+agg_check_reliability=reliability;
+FreqMHz=7250;
+confidence=50;
+mc_percentile=100;% since we're at 1 MC sim
+sim_radius_km=128; %%%%%%%%Placeholder distance         binary_dist_array=[2,4,8,16,32,64,128,256,512,1024,2048];
+base_station_latlonheight=rand_real_2025;  %%1)Lat, 2)Lon, 3)Height meters
+tf_clutter=0;%1%;  %%%%%%%????, Just do this in the EIRP reductions
+sample_spacing_km=5;
+sim_folder1='C:\Local Matlab Data\7GHz FSS Neighborhood Test Sims'
+str_tle_filename=strcat('full_geo_',num2str(rev),'.tle');
+str_website='http://www.celestrak.com/NORAD/elements/geo.txt'
+tf_opt=1; %%%%This is for the optimized move list, (not WinnForum)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+bs_eirp_reductions=(bs_eirp-bs_down_tilt_reduction)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%Create a Rev Folder
+cd(sim_folder1);
+pause(0.1)
+tempfolder=strcat('Rev',num2str(rev));
+[status,msg,msgID]=mkdir(tempfolder);
+rev_folder=fullfile(sim_folder1,tempfolder);
+cd(rev_folder)
+pause(0.1)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+maine_exception=1;  %%%%%%Just leave this to 1
+Tpol=1; %%%polarization for ITM
+deployment_percentage=100; %%%%%%%%%%%Let's not change this.
+margin=1;%%dB margin for aggregate interference
+building_loss=15;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Saving the simulation files in a folder for the option to run from a server
+'First save . . .' %%%%%24 seconds on Z drive
+tic;
+save('reliability.mat','reliability')
+save('move_list_reliability.mat','move_list_reliability')
+save('confidence.mat','confidence')
+save('FreqMHz.mat','FreqMHz')
+save('Tpol.mat','Tpol')
+save('building_loss.mat','building_loss')
+save('maine_exception.mat','maine_exception')
+save('tf_opt.mat','tf_opt')
+save('mc_percentile.mat','mc_percentile')
+save('mc_size.mat','mc_size')
+save('margin.mat','margin')
+save('deployment_percentage.mat','deployment_percentage')
+save('tf_full_binary_search.mat','tf_full_binary_search')
+save('min_binaray_spacing.mat','min_binaray_spacing')
+save('sim_radius_km.mat','sim_radius_km')
+save('bs_eirp_reductions.mat','bs_eirp_reductions')
+save('norm_aas_zero_elevation_data.mat','norm_aas_zero_elevation_data')
+save('agg_check_reliability.mat','agg_check_reliability')
+save('tf_clutter.mat','tf_clutter')
+save('base_station_latlonheight.mat','base_station_latlonheight')
+save('mitigation_dB.mat','mitigation_dB')
+toc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Pull the TLE Needed
+tic;
+outfilename=websave(str_tle_filename,str_website);
+toc;
+tleStruct=tleread(str_tle_filename);
+cell_tle=struct2cell(tleStruct);
+cell_tle_names=cellfun(@char, cell_tle(1,:), 'UniformOutput', false)';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%First loop does all the calculation for the 15 columns, then just saves the cell_sim_data for the server to make the folders
+%%%%%%%%%For Loop the Locations
+cell_data_header=cell_sim_data(1,:)
+cell_sim_data(1,:)=cell_data_header;
+
+col_data_label_idx=find(matches(cell_data_header,'data_label1'));
+col_base_polygon_idx=find(matches(cell_data_header,'base_polygon'));
+col_pp_pts_idx=find(matches(cell_data_header,'base_protection_pts'));
+col_rx_htm_idx=find(matches(cell_data_header,'rx_height'));
+col_sat_id_idx=find(matches(cell_data_header,'Sat_ID'));
+col_ele_idx=find(matches(cell_data_header,'Ground_Elevation_m'));
+col_ant_dia_idx=find(matches(cell_data_header,'ant_diamter_m'));
+col_ant_gain_idx=find(matches(cell_data_header,'rx_ant_gain_mb'));
+col_ant_pat_str_idx=find(matches(cell_data_header,'Antenna_Pattern_Str'));
+col_array_ant_pat_idx=find(matches(cell_data_header,'array_ant_pattern'));
+col_rx_noise_temp_idx=find(matches(cell_data_header,'Noise_TempK'));
+col_rx_if_bw_idx=find(matches(cell_data_header,'rx_if_bw_mhz'));
+col_in_ratio_idx=find(matches(cell_data_header,'in_ratio'));
+col_x_pol_dB_idx=find(matches(cell_data_header,'X_POL_dB'));
+col_dpa_threshold_idx=find(matches(cell_data_header,'dpa_threshold'));
+col_fdr_dB_idx=find(matches(cell_data_header,'fdr_dB'));
+col_tf_cust_ant_idx=find(matches(cell_data_header,'TF_Custom_Ant_Pattern'));
+col_gs_azimuth_idx=find(matches(cell_data_header,'gs_azimuth'));
+col_gs_elevation_idx=find(matches(cell_data_header,'gs_elevation'));
+
+
+[num_locations,~]=size(cell_sim_data);
+table([1:num_locations]',cell_sim_data(:,1))
+tic;
+for base_idx=2:1:num_locations
+    temp_single_cell_sim_data=cell_sim_data(base_idx,:);
+    data_label1=temp_single_cell_sim_data{col_data_label_idx};
+    data_label1=data_label1(find(~isspace(data_label1)));  %%%%%%%%%%Remove the White Spaces
+    cell_sim_data{base_idx,col_data_label_idx}=data_label1;
+    temp_base_polygon=temp_single_cell_sim_data{col_base_polygon_idx};
+    temp_rx_htm=temp_single_cell_sim_data{col_rx_htm_idx};
+    str_sat_name=temp_single_cell_sim_data{col_sat_id_idx};
+    temp_base_pts=temp_single_cell_sim_data{col_pp_pts_idx};
+    temp_ant_diamter=temp_single_cell_sim_data{col_ant_dia_idx};
+    temp_ant_gain=temp_single_cell_sim_data{col_ant_gain_idx};
+    temp_str_ant_pattern=temp_single_cell_sim_data{col_ant_pat_str_idx};
+    rx_temp_k=temp_single_cell_sim_data{col_rx_noise_temp_idx}
+    rx_bw_mhz=temp_single_cell_sim_data{col_rx_if_bw_idx};
+    in_ratio=temp_single_cell_sim_data{col_in_ratio_idx};
+    x_pol_dB=temp_single_cell_sim_data{col_x_pol_dB_idx};
+    fdr_dB=temp_single_cell_sim_data{col_fdr_dB_idx};
+    tf_cust_ant=temp_single_cell_sim_data{col_tf_cust_ant_idx};
+
+      
+    %%%%%%%Calcualte the DPA Threshold
+    if tf_cust_ant==1
+        dpa_threshold=-138.7+10*log10(rx_bw_mhz)+10*log10(rx_temp_k)+in_ratio+x_pol_dB+fdr_dB
+        cell_sim_data{base_idx,col_dpa_threshold_idx}=dpa_threshold;
+    else
+        'Add the normal dpa threshold calculation'
+        pause;
+    end
+
+    %%%%%%%%%%%%TLE Pull
+    sat_row_idx=find(contains(cell_tle_names,str_sat_name));
+    if isempty(sat_row_idx)
+        'Satellite TLE not found'
+        pause;
+    end
+    single_tle=tleStruct(sat_row_idx)
+
+
+    %%%%%%%%Pull in the RX (FSS) elevation
+    [num_pp_pts,~]=size(temp_base_pts)
+    if num_pp_pts>1
+        'Need to generalize for more than 1 point'
+        pause;
+    end
+
+    lat_pt=temp_base_pts(:,1);
+    lon_pt=temp_base_pts(:,2);
+    tic;
+    [matlab_ele_m]=matlab_elevation_rev1(app,lat_pt,lon_pt);
+    toc;
+    tic;
+    [elevation_m]=elevation_usgs_rev1(app,lat_pt,lon_pt);
+    toc;
+    horzcat(matlab_ele_m,elevation_m)
+    cell_sim_data{base_idx,col_ele_idx}=elevation_m;
+    %%%%%%%%%%%%%%%Set up the satellite Scenario
+    tic;
+    sampleTime=60;
+    startTime=datetime('now');
+    stopTime=startTime+seconds(sampleTime);
+    time=startTime:seconds(sampleTime):stopTime;
+    satscene=satelliteScenario(startTime,stopTime,sampleTime);
+    sat_position=propagateOrbit(datetime(time),single_tle);
+    positionTT=timetable(time',sat_position');
+    size(positionTT)
+    constellation=satellite(satscene,positionTT);
+    toc;
+    %%%%%%%%%%%%%%%%%%Create the ground stations
+    temp_altitude=elevation_m+temp_base_pts(:,3);
+    earth_ground_stat=groundStation(satscene,lat_pt,lon_pt,'Altitude',temp_altitude);
+    %%%%%%%%%%%%%%%Calculate the azimuth and elevation from the Earth to the Satellite
+    [gs_azimuth,gs_elevation,gs_range]=aer(earth_ground_stat,constellation,stopTime)
+
+
+    cell_sim_data{base_idx,col_gs_azimuth_idx}=gs_azimuth;
+    cell_sim_data{base_idx,col_gs_elevation_idx}=gs_elevation;
+    strcat(num2str(base_idx/num_locations*100),'%')
+end
+toc;
+
+
+cd(rev_folder)
+pause(0.1)
+cell_sim_data(1:2,:)'
+'Last save . . .'
+tic;
+save('cell_sim_data.mat','cell_sim_data')
+toc;
+
+cell_ppt_size=cellfun(@size,cell_sim_data(:,col_pp_pts_idx),'UniformOutput',false);
+temp_ppt_size=cell2mat(cellfun(@size,cell_sim_data(:,col_pp_pts_idx),'UniformOutput',false));
+cell_sim_data'
+%sort(temp_ppt_size(:,1))
+
+
+horzcat(cell_sim_data(:,1),cell_ppt_size)
+max(temp_ppt_size(:,1))
+cell_sim_data(:,[1,5,6,7,8,14,16,17,21])
+size(cell_sim_data)
+rev_folder
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Now running the simulation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tf_server_status=0;
+parallel_flag=0%1%0;
+[workers,parallel_flag]=check_parallel_toolbox(app,parallel_flag)
+workers=2
+tf_recalculate=0%1%0%1
+tf_rescrap_rev_data=0%1
+tf_print_excel=1
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+neighborhood_wrapper_rev6_miti_geoplots_pea_custant_excel(app,rev_folder,parallel_flag,tf_server_status,workers,tf_recalculate,tf_rescrap_rev_data,tf_print_excel)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end_clock=clock;
+total_clock=end_clock-top_start_clock;
+total_seconds=total_clock(6)+total_clock(5)*60+total_clock(4)*3600+total_clock(3)*86400;
+total_mins=total_seconds/60;
+total_hours=total_mins/60;
+if total_hours>1
+    strcat('Total Hours:',num2str(total_hours))
+elseif total_mins>1
+    strcat('Total Minutes:',num2str(total_mins))
+else
+    strcat('Total Seconds:',num2str(total_seconds))
+end
+cd(folder1)
+'Done'
+
+
+
+
+
+
+
+
+
